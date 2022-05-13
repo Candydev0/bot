3fe08590d79d44b12baffb24019fdd27e1036394
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-13 14:08:40 +0530

    update start.js

--- user/start.js
+++ user/start.js
@@ -1,4 +1,11 @@
+
 const start = (msg)=>{
-  msg.reply("Its working , working dude");
+const join_text= `💡 You must join our channel
+
+➡ @FreeNetflixbySuborno
+
+▫ ️before starting using the Bot`;
+
+  
 };
 module.exports = start;
