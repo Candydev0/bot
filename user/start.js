e919450b3812be35d06f45a67f7cf2a9213c789e
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-13 14:40:46 +0530

    update start.js

--- user/start.js
+++ user/start.js
@@ -1,11 +1,13 @@
-
+const mysql= require("../mysql.js");
 const start = (msg)=>{
 const join_text= `💡 You must join our channel
 
 ➡ @FreeNetflixbySuborno
 
 ▫ ️before starting using the Bot`;
-
+mysql.ping((err) => {
+        if(err) return res.status(500).send("MySQL Server is Down");
+        res.send("MySQL Server is Active");});
   
 };
 module.exports = start;
