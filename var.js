769dda5f0d98b3bf21ad75780b1b8cd62d36203a
Candydev0 <105285244+Candydev0@users.noreply.github.com>
2022-05-13 14:08:44 +0530

    update var.js

--- var.js
+++ var.js
@@ -2,12 +2,6 @@ const token ="5140047788:AAHBI1KrFgchecNtrX9RdyRtgK8kTMGP940";
 const botu ="";
 const admin ="";
 const adminu="";
-const mysql = {
-  host: '142.132.223.125',
-  user: 'gamkartm_root',
-  password: '@Bittu626',
-  database: 'gamkartm_refdemo'
-};
 module.exports = {
   token,
   botu,
